4d1c187827ff11be8cca67deeef5d2cb424b8d96
agrajjain4786 <agrajjain4786@gmail.com>
2025-08-15 18:15:30 +0530

    if statement

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+// if statement
+
+let color = "yellow";
+
+if (color == "red") {
+  console.log(`stop light is ${color}`);
+}
+if (color == "yellow") {
+  console.log(`go slow light is ${color}`);
+}
+if (color == "green") {
+  console.log(`go light is ${color}`);
+}
+
