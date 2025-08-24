3412848c9c6f8a9845d701e27417636d4f37e0ad
agrajjain4786 <agrajjain4786@gmail.com>
2025-08-24 12:53:20 +0530

    posts plateform using restful api

---
+++ REST_CLASS/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const methodOverride = require("method-override");
+const app = express();
+const port = 8080;
+
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "instagram",
+    content: "Reel scrole",
+  },
+  {
+    id: uuidv4(),
+    username: "youtube",
+    content: "shorts scrole",
+  },
+  {
+    id: uuidv4(),
+    username: "internshala",
+    content: "job scrole",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("serving working well!");
+});
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+  console.log(post);
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  // res.send(`patch request working`);
+  res.redirect("/posts/");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render(`edit.ejs`, { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id); // save only those post which id != id that was delete
+  res.redirect(`/posts/`);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
