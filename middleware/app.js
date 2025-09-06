e81495236d7f7fa3e68894fbdf93a506d3b5d019
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-06 15:17:42 +0530

    try first middleware

--- middleware/app.js
+++ middleware/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 
 // middleware
+app.use((req, res) => {
+  console.log(`Hi, I am middleware`);
+  res.send("middleware finished");
+});
 
 app.get("/", (req, res) => {
-  res.send("something");
+  res.send("hi i am root");
+});
+
+app.get("/random", (req, res) => {
+  res.send("hi i am random page");
 });
 
 const port = 8080;
