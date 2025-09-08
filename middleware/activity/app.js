de5d3657772a2b3094e8e22a87fef4be8496bb7d
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-08 16:53:32 +0530

    activity error

--- middleware/activity/app.js
+++ middleware/activity/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("../ErrorHandling/ExpressError");
 
 app.use("/api", (req, res, next) => {
   let { token } = req.query;
@@ -7,6 +8,15 @@ app.use("/api", (req, res, next) => {
     return next();
   }
   res.send("ACCESS DENIED!");
+});
+
+app.get("/admin", (req, res) => {
+  throw new ExpressError(403, `ACCESS TO ADMIN IS FORBIDDEN`);
+});
+
+app.use((err, req, res, next) => {
+  let { status = 500, message = `some error` } = err;
+  res.status(status).send(message);
 });
 
 app.get("/api", (req, res) => {
